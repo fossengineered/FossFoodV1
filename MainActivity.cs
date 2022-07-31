080d6f85a0282460d2c4aaa34243e9e89d7b753d
fossengineered <99218408+fossengineered@users.noreply.github.com>
2022-07-31 17:20:29 -0400

    hide keyboard

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,6 +1,11 @@
 ﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
+using Android.Views;
+using Android.Views.InputMethods;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 using FossFoodV1.Orders;
 using System;
@@ -18,7 +23,59 @@ namespace FossFoodV1
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.order_main);
 
+           
             _ordersVM = new OrdersViewModels(this);
+        }
+
+        protected void SetupParent(View view)
+        {
+            //Set up touch listener for non-text box views to hide keyboard.
+            if (view.GetType() != typeof(EditText))
+            {
+                view.Touch += (sender, e) =>
+                {
+                    var inputManager = (InputMethodManager)GetSystemService(InputMethodService);
+                    inputManager.HideSoftInputFromWindow(CurrentFocus?.WindowToken, HideSoftInputFlags.None);
+                };
+            }
+
+            //If a layout container, iterate over children
+            if (view.GetType() != typeof(ViewGroup))
+            {
+                var v = (ViewGroup)view;
+                for (int i = 0; i < v.ChildCount; i++)
+                {
+                    View innerView = ((ViewGroup)view).GetChildAt(i);
+                    SetupParent(innerView);
+                }
+            }
+        }
+
+        public override bool OnTouchEvent(MotionEvent? ev)
+        {
+            var inputManager = (InputMethodManager)GetSystemService(InputMethodService);
+            inputManager.HideSoftInputFromWindow(CurrentFocus?.WindowToken, HideSoftInputFlags.None);
+
+            return base.OnTouchEvent(ev);
+
+            //if (ev.Action != MotionEventActions.Down)
+            //    return base.OnTouchEvent(ev);
+
+            //if (!(CurrentFocus is EditText))
+            //    return base.OnTouchEvent(ev);
+
+            //Rect outRect = new Rect();
+            //CurrentFocus.GetGlobalVisibleRect(outRect);
+
+            //if (outRect.Contains((int)ev.RawX, (int)ev.RawY))
+            //    return base.OnTouchEvent(ev);
+
+            //CurrentFocus.ClearFocus();
+
+            ////InputMethodManager imm = (InputMethodManager)GetSystemService(Context.InputMethodService);
+            ////imm.HideSoftInputFromWindow(CurrentFocus?.WindowToken, 0);
+
+            //return base.OnTouchEvent(ev);
         }
 
         private void ContentMain_Click(object sender, EventArgs e)
